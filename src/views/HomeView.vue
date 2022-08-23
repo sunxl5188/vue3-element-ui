f08db66a7bc1d19e971518ebe7f4fa4b464e12a9
364864264 <364864264@qq.com>
2022-08-23 18:00:44 +0800

    提交

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,35 +1,49 @@
 <template>
-  <div>
-    <el-card class="box-card">
-      <template #header>
-        <div class="card-header">
-          <span>Card name</span>
-          <el-button class="button" text>Operation button</el-button>
-        </div>
-      </template>
-      <div v-for="o in 4" :key="o" class="text item">
-        {{ "List item " + o }}
-      </div>
-    </el-card>
+  <div @click="data.girlFun($event)">
+    <el-button v-for="(o, index) in data.girl" :key="o" type="primary">
+      <div :data-id="index">{{ "List item " + o }}</div>
+    </el-button>
+    <h6>{{ data.selectGirl }}</h6>
   </div>
 </template>
 
 <script lang="ts">
-import { ref } from "vue";
+import {
+  reactive,
+  onBeforeMount,
+  onMounted,
+  onUnmounted,
+  onBeforeUpdate,
+  onUpdated,
+} from "vue";
 
 export default {
   setup() {
-    const girl = ref(["大脚", "小闫", "小妹"]);
-    const selectGirl = ref("");
-    const girlFun = ref((e: any) => {
-      // selectGirl.value = girl.value[index];
-      console.log(e.target.dataset.id);
-      console.log(e);
+    console.log("1-开始创建组件-----setup()");
+    const data = reactive({
+      girl: ["大脚", "小闫", "小妹"],
+      selectGirl: "",
+      girlFun: (e: any) => {
+        data.selectGirl = data.girl[e.target.dataset.id];
+      },
+    });
+    onBeforeMount(() => {
+      console.log("2-组件挂载到页面之前执行-----onBeforeMount");
+    });
+    onMounted(() => {
+      console.log("3-组件挂载到页面之后执行-----onMounted");
+    });
+    onUnmounted(() => {
+      console.log("4-在组件实例被卸载之后执行-----onUnmounted");
+    });
+    onBeforeUpdate(() => {
+      console.log("5-组件状态变更而更新其 DOM 树之前执行===>onBeforeUpdate");
+    });
+    onUpdated(() => {
+      console.log("6-组件更新后调用---onUpdated");
     });
     return {
-      girl,
-      selectGirl,
-      girlFun,
+      data,
     };
   },
 };
