515bf66859efb8474abc08641a10ad58a9b7da3d
364864264 <364864264@qq.com>
2022-08-23 21:21:27 +0800

    vue3

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,32 +1,49 @@
 <template>
-  <div @click="data.girlFun($event)">
-    <el-button v-for="(o, index) in data.girl" :key="o" type="primary">
+  <div>
+    <el-button
+      v-for="(o, index) in girl"
+      :key="o"
+      @click="girlFun(index)"
+      type="primary"
+    >
       <div :data-id="index">{{ "List item " + o }}</div>
     </el-button>
-    <h6>{{ data.selectGirl }}</h6>
+    <h6>{{ selectGirl }}</h6>
   </div>
 </template>
 
 <script lang="ts">
 import {
   reactive,
+  toRefs,
   onBeforeMount,
   onMounted,
   onUnmounted,
   onBeforeUpdate,
   onUpdated,
+  onBeforeUnmount,
+  onErrorCaptured,
 } from "vue";
+
+interface dataProps {
+  girl: string[];
+  selectGirl: string;
+  girlFun: (index: number) => void;
+}
 
 export default {
   setup() {
     console.log("1-开始创建组件-----setup()");
-    const data = reactive({
+    const data: dataProps = reactive({
       girl: ["大脚", "小闫", "小妹"],
       selectGirl: "",
-      girlFun: (e: any) => {
-        data.selectGirl = data.girl[e.target.dataset.id];
+      girlFun: (id: number) => {
+        data.selectGirl = data.girl[id];
       },
     });
+
+    const refData = toRefs(data);
+
     onBeforeMount(() => {
       console.log("2-组件挂载到页面之前执行-----onBeforeMount");
     });
@@ -34,7 +51,7 @@ export default {
       console.log("3-组件挂载到页面之后执行-----onMounted");
     });
     onUnmounted(() => {
-      console.log("4-在组件实例被卸载之后执行-----onUnmounted");
+      console.log("7-在组件实例被卸载之后执行-----onUnmounted");
     });
     onBeforeUpdate(() => {
       console.log("5-组件状态变更而更新其 DOM 树之前执行===>onBeforeUpdate");
@@ -42,8 +59,14 @@ export default {
     onUpdated(() => {
       console.log("6-组件更新后调用---onUpdated");
     });
+    onBeforeUnmount(() => {
+      console.log("4-组件实例被卸载之前调用--->onBeforeUnmount");
+    });
+    onErrorCaptured((err) => {
+      console.log("捕获了后代组件传递的错误时调用", err);
+    });
     return {
-      data,
+      ...refData,
     };
   },
 };
